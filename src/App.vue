caa7980e0c6497327f812edb5e7d3c0b6b5d2dad
Devin Ward <devinw1325wc@gmail.com>
2020-03-10 20:31:27 -0500

    First commit. Scaffolding.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <MarkdownEditor id="myEditor" />
+  </div>
+</template>
+
+<script>
+import MarkdownEditor from "./components/MarkdownEditor.vue";
+import "./assets/css/main.css";
+
+export default {
+  name: "app",
+  components: {
+    MarkdownEditor
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: darkcyan;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0px;
+}
+#myEditor {
+  background-color: white;
+}
+</style>
---
+++ src/components/MarkdownEditor.vue
@@ -0,0 +1,22 @@
+<template>
+    <editor v-model="editorText" height="1000px" />
+</template>
+
+<script>
+import 'tui-editor/dist/tui-editor.css'
+import 'tui-editor/dist/tui-editor-contents.css'
+import 'codemirror/lib/codemirror.css'
+import { Editor } from '@toast-ui/vue-editor'
+
+export default {
+    name: 'Markdown Editor',
+    data () {
+        return {
+            editorText: ''
+        }
+    },
+    components: {
+        'editor': Editor
+    }
+}
+</script>
